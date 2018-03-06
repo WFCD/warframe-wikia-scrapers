7794476154f72f6a16f3a7ac168865d85065a4ca
Tobiah <tobiah@protonmail.com>
2018-03-06 22:41:26 +0000

    attempt to auto-deploy changes

---
+++ .travis/push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "reporting" ]; then
+  setup_git() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+  }
+ 
+  prepare_json() {
+    node index.js
+  }
+
+  commit_worldstate_data_files() {
+    cd ../
+    git clone git@github.com:WFCD/warframe-worldstate-data.git
+    cd warframe-worldstate-data
+    git checkout -b scraper-travis-test
+    cp ../warframe-wikia-scrapers/build/weapondatafinal.json data/weapons.json
+    git add data/weapons.json
+    git commit --message "Travis build: warframe-wikia-scrapers $TRAVIS_BUILD_NUMBER"
+  }
+    
+  upload_files() {
+    git remote add origin-update https://${GH_TOKEN}@github.com/WFCD/warframe-worldstate-data.git > /dev/null 2>&1
+    git push --quiet --set-upstream  origin-update scraper-travis-test
+  }
+    
+  setup_git
+  prepare_json
+  commit_worldstate_data_files
+  upload_files
+fi
