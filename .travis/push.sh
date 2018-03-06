6b167bdd5f8d02de895a2a638025b77eeb4681a6
Tobiah <tobiah@protonmail.com>
2018-03-06 23:13:29 +0000

    clean up shell script

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,9 +3,9 @@ if [ "$TRAVIS_BRANCH" == "reporting" ]; then
   setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
-    git clone git@github.com:WFCD/warframe-worldstate-data.git
+    git clonehttps://github.com/WFCD/warframe-worldstate-data.git
     cd warframe-worldstate-data
-    git checkout -b scraper-travis-test
+    git checkout -b travis-$TRAVIS_BUILD_NUMBER
   }
  
   prepare_json() {
@@ -13,9 +13,6 @@ if [ "$TRAVIS_BRANCH" == "reporting" ]; then
   }
 
   commit_worldstate_data_files() {
-    git clone https://github.com/WFCD/warframe-worldstate-data.git
-    cd warframe-worldstate-data
-    git checkout -b travis-$TRAVIS_BUILD_NUMBER
     cp ../build/weapondatafinal.json data/weapons.json
     git add data/weapons.json
     git commit --message "Travis build: warframe-wikia-scrapers $TRAVIS_BUILD_NUMBER"
