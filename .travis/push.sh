67072241e691ee53a1e5c0499b598d6a787aa4cf
Tobiah <tobiah@protonmail.com>
2018-03-07 21:36:23 +0000

    add code to push and update warframe-status

--- .travis/push.sh
+++ .travis/push.sh
@@ -9,6 +9,7 @@ setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   git clone https://github.com/WFCD/warframe-worldstate-data.git
+  git clone https://github.com/WFCD/warframe-status.git
 }
 
 prepare_json() {
@@ -28,7 +29,27 @@ upload_files() {
   git push --quiet --set-upstream  origin-update $BRANCH
 }
 
+publish_worldstate_data() {
+  npm version patch -m "Travis build $TRAVIS_BUILD_NUMBER update from warframe-wikia-scrapers"
+}
+
+# Update wfcd/warframe-status to consume the new version of warframe-worldstate-data
+update_warframe_status() {
+  cd ../warframe-status
+  git checkout -b $BRANCH
+  npm update --save
+  git add package.json
+  git commit --message "Travis build: warframe-wikia-scrapers $TRAVIS_BUILD_NUMBER"
+  git remote add origin-update https://${GH_TOKEN}@github.com/WFCD/warframe-status.git
+  git push --quiet --set-upstream  origin-update $BRANCH
+}
+
 setup_git
 prepare_json
 commit_worldstate_data_files
 upload_files
+
+if [ "$BRANCH" == "master" ]; then
+  publish_worldstate_data
+  update_warframe_status
+fi
