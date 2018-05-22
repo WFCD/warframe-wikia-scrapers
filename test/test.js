898ece2e1004572d306cbf8e131e8ece5ea8b9d3
mike0891 <mikeholt91@gmail.com>
2018-05-22 17:04:11 +0100

    fix:tests for warframe.

--- test/test.js
+++ test/test.js
@@ -10,9 +10,11 @@ const { assert } = require('chai');
 
 let weaponDataRaw;
 let weaponData;
+let warframedataRaw;
+let WarframeData;
 
 describe('Output file', () => {
-  it('should contain a valid, nonempty JSON array of objects:', async () => {
+  it('should contain a valid, nonempty JSON array of objectsfor weapons:', async () => {
     try {
       weaponDataRaw = await fs.readFile('build/weapondatafinal.json');
     } catch (err) {
@@ -30,5 +32,26 @@ describe('Output file', () => {
   after(() => {
     console.log();
     console.dir(weaponData[0]);
+  });
+});
+describe('Output file', () => {
+  it('should contain a valid, nonempty JSON array of objects for frames:', async () => {
+    try {
+      warframedataRaw = await fs.readFile('build/framedatafinal.json');
+    } catch (err) {
+      assert.fail(err);
+    }
+
+    try {
+      WarframeData = JSON.parse(warframedataRaw);
+      assert.typeOf(WarframeData[0], 'object', 'json ');
+    } catch (err) {
+      assert.fail(err);
+    }
+  });
+
+  after(() => {
+    console.log();
+    console.dir(WarframeData[0]);
   });
 });
